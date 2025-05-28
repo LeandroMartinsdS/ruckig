0e34b0ad1749c2767fec1263f353490baf83306f
Leandro Martins dos Santos <Leandro.Martins-dos-Santos@diamond.ac.uk>
2025-05-28 15:19:41 +0000

    Redefine trajectory and file formatting
    Spiral starting from center
    Acceleration and jerk limited
    Add time at the file's first column

--- examples/spiral.py
+++ examples/spiral.py
@@ -16,17 +16,17 @@ if __name__ == '__main__':
     out = OutputParameter(2)
     enc_res = 10e6
     # Set input parameters       r  , theta
-    inp.current_position =      [2.5, 4*np.pi] # mm, rad
+    inp.current_position =      [0.0, 0.0] # mm, rad
     inp.current_velocity =      [0.0, 0.0]
     inp.current_acceleration =  [0.0, 0.0]
 
-    inp.target_position         = [0.0, 0.0] # mm, rad
+    inp.target_position         = [2.5, 4*np.pi] # mm, rad
     inp.target_velocity         = [0.0, 0.0]
     inp.target_acceleration     = [0.0, 0.0]
 
     inp.max_velocity = [0.1, 0.4*np.pi]
-    # inp.max_acceleration = [0.06, 0.1*np.pi]
-    # inp.max_jerk = [0.06, 0.1*np.pi]
+    inp.max_acceleration = [0.025, 0.1*np.pi]
+    inp.max_jerk = [0.005, 0.02*np.pi]
 #####################################
 
     # Generate the trajectory within the control loop
@@ -40,6 +40,7 @@ if __name__ == '__main__':
     cartesian_positions=[]
     cartesian_velocities=[]
     cartesian_accelerations=[]
+
     with open(os.path.join(dir_path, filename), 'w', newline='') as file:
         writer = csv.writer(file)
 
@@ -55,13 +56,13 @@ if __name__ == '__main__':
 
             x   = enc_res * (r * np.cos(theta))
             dx  = enc_res * (dr * np.cos(theta) - r * dtheta * np.sin(theta))
-            ddx = enc_res * (((ddr * np.cos(theta)) - (dr * dtheta * np.sin(theta))) - ((dr * dtheta + r * ddtheta) * np.sin(theta) + (r * dtheta * dtheta * np.cos(theta))))
+            ddx = enc_res * (ddr * np.cos(theta) - 2 * dr * dtheta * np.sin(theta) - r * ddtheta * np.sin(theta) - r * dtheta**2 * np.cos(theta))
 
             y   = enc_res*(r * np.sin(theta))
             dy  = enc_res*(dr * np.sin(theta) + r * dtheta * np.cos(theta))
-            ddy = enc_res*(((ddr * np.sin(theta)) + (dr * dtheta * np.cos(theta))) + ((dr * dtheta + r * ddtheta) * np.cos(theta) - (r * dtheta * dtheta * np.sin(theta))))
+            ddy = enc_res * (ddr * np.sin(theta) + 2 * dr * dtheta * np.cos(theta) + r * ddtheta * np.cos(theta) - r * dtheta**2 * np.sin(theta))
 
-            writer.writerow([int(x), int(y)])
+            writer.writerow([int(out.time*1000), int(x), int(y), 0, 0, 0, 0, 0, 0])
             cartesian_positions.append([x, y])
             cartesian_velocities.append([dx, dy])
             cartesian_accelerations.append([ddx, ddy])
@@ -88,7 +89,7 @@ if __name__ == '__main__':
 
     project_path = Path(__file__).parent.parent.absolute()
 
-    Plotter.plot_trajectory(project_path / 'examples' / '01-x_trajectory.pdf', otg, inp, out_list, plot_acceleration=True, plot_jerk=True, show=True)
+    Plotter.plot_trajectory(project_path / 'examples' / '01-x_trajectory.pdf', otg, inp, out_list, plot_acceleration=True, plot_jerk=False, show=True)
     Plotter.plot_cartesian(cartesian_positions)
     Plotter.plot_cart_traj(out_list, otg.delta_time, cartesian_positions, cartesian_velocities, cartesian_accelerations, True)
     import matplotlib.pyplot as plt
